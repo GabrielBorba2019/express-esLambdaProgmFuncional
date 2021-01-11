75c0a8010fb067b822bc607be2112b573260c256
GabrielBorba2019 <gabrielborba.pm@Gmail.com>
2021-01-11 18:39:01 -0300

    Function in Function

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import entities.Product;
+import util.ProductService;
 
 public class Program {
 
@@ -20,12 +19,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//Expressao Lambda INLINE
-	
+		ProductService ps = new ProductService();
 		
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+		System.out.println("Sum = " + String.format("%.2f", sum));
 		
-		names.forEach(System.out::println);
 		
 	}
 }
