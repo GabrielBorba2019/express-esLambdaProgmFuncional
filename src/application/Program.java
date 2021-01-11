0898c6e5fc2e97863747e3b6e052ac168efe8501
GabrielBorba2019 <gabrielborba.pm@Gmail.com>
2021-01-11 17:57:39 -0300

    Lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,15 +19,12 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//Expressao Lambda declarada
+		//Expressao Lambda inline
 		double factor = 1.1;
 		
-		Consumer<Product> cons = p -> {
-			p.setPrice(p.getPrice() * factor);
-		};
 		
 		//Chamo o variável cons
-		list.forEach(cons);
+		list.forEach( p -> p.setPrice(p.getPrice() * factor));
 		
 		
 		list.forEach(System.out::println);
