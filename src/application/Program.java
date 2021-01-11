277494e0847f872fd518060c7b015b85d1f0d62f
GabrielBorba2019 <gabrielborba.pm@Gmail.com>
2021-01-11 17:49:16 -0300

    Costumer Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,17 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Locale;
+
 
 import entities.Product;
+import util.PriceUpdate;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("Tv", 900.00));
@@ -17,12 +20,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//Expressao LAMBDA inline
-		list.removeIf(p -> p.getPrice() >= 100.0);
+		//Consumer Interface. passa como parametro um objeto instanciado do tipo Consumer(Interface Funcional)
+		list.forEach(new PriceUpdate());
 		
-		for (Product p : list) {
-			System.out.println(p);
-		}
-	
+		list.forEach(System.out::println);
 	}
 }
